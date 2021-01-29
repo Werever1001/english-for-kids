0d74dbb82bbf1a6ae66501be564054d47479142f
Yana <werever@inbox.ru>
2021-01-29 21:57:47 +0300

    feat: improve code

--- src/js/button.js
+++ src/js/button.js
@@ -1,19 +1,12 @@
-"use strict"
-
 export default class Button {
-  constructor (button) {
-    this.button = button;
+  constructor () {
+    this.button = true;
   }
-
   create() {
     let button = document.querySelector('.toggle-switch');
-    let cardMode = document.querySelectorAll('.interface');
-    let areaMode = document.querySelector('train');
+    let trainMode = document.querySelector('.train');
     button.onclick = function() {
-      areaMode.classList.toggle('game');
-      cardMode.forEach(elem => {
-        elem.classList.toggle('play');
-      })            
+      trainMode.classList.toggle('game');           
     }
   }
 }
